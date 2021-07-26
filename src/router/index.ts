6e7e13af1bc4ddcedd6e842932e78c1796880fbd
kurianbenoy-aot <kurian.benoy@aot-technologies.com>
2021-07-26 19:21:01 +0530

    remove node-sass application

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
+import TaskList from '../views/TaskList.vue'
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'TaskList',
+    component: TaskList
   },
   {
     path: '/about',
